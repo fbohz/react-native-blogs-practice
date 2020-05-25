d04cdf04fd7bc25e5231f252e3917df710d469c7
Felipe <fb.dc.col@gmail.com>
2020-05-25 12:08:07 -0500

    update

--- 1.Demo/src/screens/_screenTemplate.js
+++ 1.Demo/src/screens/_screenTemplate.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Text, StyleSheet, View } from "react-native";
 
 const Name = () => {
-    return <Text>Test</Text>
+    return (
+        <View>
+            <Text>Template</Text>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-
+ 
 })
 
 export default Name
